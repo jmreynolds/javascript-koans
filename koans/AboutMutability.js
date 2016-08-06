718605de7ccd53849b89f907181fb8cb76f5abee
Joseph Reynolds <jreynolds@infocraft.net>
2016-08-06 08:40:42 -0500

    30 Koans Know AboutMutability Complete

--- koans/AboutMutability.js
+++ koans/AboutMutability.js
@@ -54,15 +54,19 @@ describe("About Mutability", function() {
     aPerson.lastname = "Andrews";
     aPerson.fullName = "Penny Andrews";
 
-    expect(aPerson.getFirstName()).toBe(FILL_ME_IN);
-    expect(aPerson.getLastName()).toBe(FILL_ME_IN);
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    expect(aPerson.getFirstName()).toBe('John');
+    expect(aPerson.getLastName()).toBe('Smith');
+    expect(aPerson.getFullName()).toBe('John Smith');
 
     aPerson.getFullName = function () {
       return aPerson.lastname + ", " + aPerson.firstname;
     };
 
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    // TODO: So, to be clear...
+    // firstname and lastname are private because they are defined in 'Person'
+    // getFullName is mutable because it is defined in aPerson
+    // right?
+    expect(aPerson.getFullName()).toBe('Andrews, Penny');
   });
 
 });
